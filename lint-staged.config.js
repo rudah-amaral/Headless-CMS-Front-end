8e1dcb5251258ff3edc293ef2635ac942f656a79
Rudah Amaral <19709437+rudah-amaral@users.noreply.github.com>
2024-03-08 18:03:35 -0300

    Fix: Stop triggering warnings for ignored files
    This just happens because I'm using ESLint with husky and lint-staged.
    (https://stackoverflow.com/questions/37927772/how-to-silence-warnings-about-ignored-files-in-eslint)

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,8 +1,20 @@
+import { ESLint } from "eslint";
+
+const removeIgnoredFiles = async (files) => {
+  const eslint = new ESLint();
+  const ignoredFiles = await Promise.all(
+    files.map((file) => eslint.isPathIgnored(file)),
+  );
+  const filteredFiles = files.filter((_, i) => !ignoredFiles[i]);
+  return filteredFiles.join(" ");
+};
+
 export default {
-  "**/*.ts?(x)": (filenames) => {
+  "**/*.ts?(x)": async (filenames) => {
     const styleFix = `prettier --write ${filenames.join(" ")}`;
     const typeCheck = "tsc -p tsconfig.json --noEmit";
-    const lintCheck = `eslint --max-warnings=0 ${filenames.join(" ")}`;
+    const filesToLint = await removeIgnoredFiles(filenames);
+    const lintCheck = `eslint --max-warnings=0 ${filesToLint}`;
     const styleCheck = `prettier --check ${filenames.join(" ")}`;
     const cmd = 'concurrently -c "auto" -n "type-check,lint-check,style-check"';
     return [styleFix, `${cmd} "${typeCheck}" "${lintCheck}" "${styleCheck}"`];
